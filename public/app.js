758a66ae5940ebcaf2b4930bb9f89e049719ec9a
Tanner Swenson <TannerSwenson@gmail.com>
2014-03-06 18:55:00 -0800

    Made dependencies local and made jasmine load underscore first

--- public/app.js
+++ public/app.js
@@ -2,20 +2,20 @@ window.App = {
   Models: {},
   Collections: {},
   Views: {},
-  Routers: {},  
+  Routers: {},
   initialize: function() {
     this.router = new this.Routers.Main();
     Backbone.history.start({pushState: true});
 
     App.autocompleter = new Autocompleter();
     var ws = new WebSocket('ws://' + window.location.host + window.location.pathname);
-    ws.onmessage = function(m) { 
-      autocompleter.add(m.data); 
+    ws.onmessage = function(m) {
+      autocompleter.add(m.data);
     };
 
   }
 };
-$(document).ready(function(){
+setTimeout(function(){$(document).ready(function(){
   App.initialize();
-});
+});}, 500);
 
